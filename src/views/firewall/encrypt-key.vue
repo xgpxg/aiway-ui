82f035db4ef57062c6ae6b081b92e3233afe5c58
wxg <1584929962@qq.com>
2026-01-15 10:44:25 +0800

    feat(firewall): 添加API密钥加密功能并重构防火墙界面

---
+++ src/views/firewall/encrypt-key.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+
+import {ElMessage, ElMessageBox} from "element-plus";
+import {R} from "../../utils/R";
+import {U} from "../../utils/util"
+
+const value = defineModel()
+
+const rules = {
+  api_secret_encrypt_key: [
+    {
+      required: true,
+      message: '请输入API Key加密密钥',
+      trigger: 'blur'
+    },
+    {
+      min: 32,
+      max: 32,
+      message: 'API Key加密密钥长度为32位',
+      trigger: 'blur'
+    },
+    {
+      pattern: /^[\x00-\x7F]*$/,
+      message: 'API Key加密密钥只能是ascii字符',
+      trigger: 'blur'
+    }
+  ]
+}
+const old = value.value['api_secret_encrypt_key']
+
+const update = () => {
+  ElMessageBox.confirm('修改后将导致已创建的API Key无法使用，请确认操作', '确定修改加密密钥', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  }).then(() => {
+    R.postJson('/api/firewall/update', value.value).then(res => {
+      if (res.code === 0) {
+        ElMessage.success('密钥已更新')
+      }
+    })
+  }).catch(() => {
+    value.value['api_secret_encrypt_key'] = old
+  })
+}
+
+const randomGen = () => {
+  value.value['api_secret_encrypt_key'] = U.randomString(32)
+}
+</script>
+
+<template>
+  <el-form :model="value" :rules="rules" label-position="left" label-width="140" @submit.prevent>
+    <el-form-item label="API Key加解密密钥" prop="api_secret_encrypt_key">
+      <el-input class="half-width" placeholder="请输入API Key加密密钥，长度固定为32位"
+                v-model="value['api_secret_encrypt_key']" minlength="32" maxlength="32" show-word-limit>
+      </el-input>
+      <el-button @click="randomGen" class="ml10">随机生成</el-button>
+      <el-text type="info" size="small" class="fill-width">
+        API Key密钥用于API Key的加密/解密，请妥善保存。该密钥请不要频繁修改，修改后将导致已创建的API Key无法使用。
+      </el-text>
+    </el-form-item>
+    <el-form-item label="">
+      <el-button type="primary" @click="update">保存</el-button>
+    </el-form-item>
+  </el-form>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/firewall/index.vue
+++ src/views/firewall/index.vue
@@ -1,24 +1,36 @@
 <script setup lang="ts">
 import {onMounted, ref, watch} from "vue";
+import {useRoute, useRouter} from "vue-router";
 import IpPolicy from "./ip-policy.vue";
 import RefererPolicy from "./referer-policy.vue";
 import {R} from "../../utils/R";
 import Traffic from "./traffic.vue";
+import EncryptKey from "./encrypt-key.vue";
 
-const activeIndex = ref('100');
+const route = useRoute();
+const router = useRouter();
+
+const activeIndex = ref('ip-policy');
 
 const firewall = ref({})
 
 onMounted(() => {
-  loadFirewall()
+  // 从路由参数获取当前激活的标签页，如果没有则默认为 'ip-policy'
+  const tab = route.query.tab || 'ip-policy';
+  activeIndex.value = tab;
+  loadFirewall();
 })
+
 const loadFirewall = () => {
   R.get('/api/firewall/detail').then(res => {
     firewall.value = res.data
   })
 }
-watch(activeIndex, () => {
-  loadFirewall()
+
+watch(activeIndex, (newTab) => {
+  // 更新路由参数，但不刷新页面
+  router.push({query: {...route.query, tab: newTab}});
+  loadFirewall();
 })
 </script>
 
@@ -27,30 +39,36 @@ watch(activeIndex, () => {
     <el-row :gutter="10">
       <el-col :span="3">
         <el-menu
-            default-active="100"
+            default-active="ip-policy"
             class="el-menu-vertical-demo"
             style="width: 130px"
             @select="(index) =>{activeIndex = index}"
         >
           <el-menu-item-group title="访问策略">
-            <el-menu-item index="100">
+            <el-menu-item index="ip-policy">
               IP访问策略
             </el-menu-item>
-            <el-menu-item index="101">
+            <el-menu-item index="referer-policy">
               Referer策略
             </el-menu-item>
           </el-menu-item-group>
           <el-menu-item-group title="流量控制">
-            <el-menu-item index="200">
+            <el-menu-item index="traffic">
               网关流量
+            </el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="密钥设置">
+            <el-menu-item index="encrypt-key">
+              加密密钥
             </el-menu-item>
           </el-menu-item-group>
         </el-menu>
       </el-col>
       <el-col :span="21">
-        <ip-policy v-model="firewall" v-if="activeIndex === '100'"></ip-policy>
-        <referer-policy v-model="firewall" v-if="activeIndex === '101'"></referer-policy>
-        <traffic v-model="firewall" v-if="activeIndex === '200'"></traffic>
+        <ip-policy v-model="firewall" v-if="activeIndex === 'ip-policy'"></ip-policy>
+        <referer-policy v-model="firewall" v-if="activeIndex === 'referer-policy'"></referer-policy>
+        <traffic v-model="firewall" v-if="activeIndex === 'traffic'"></traffic>
+        <encrypt-key v-model="firewall" v-if="activeIndex === 'encrypt-key'"></encrypt-key>
       </el-col>
     </el-row>
   </div>
